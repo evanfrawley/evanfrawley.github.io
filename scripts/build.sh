c702188fc8e5f7cc04f06670a82dedcfd1d84a0b
evanfrawley <frawley@uw.edu>
2017-10-16 23:15:35 -0700

    add build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 docker build -t evanfrawley/personalsite .
-docker push evanfrawley/personalsite
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+docker push evanfrawley/personalsite
 ssh -oStrictHostKeyChecking=no root@$PERSONAL_SITE_IP 'bash -s' < $(pwd)/scripts/redeploy.sh
