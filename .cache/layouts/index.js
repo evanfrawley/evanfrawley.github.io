9ce13c66f01151ca01e017c1e2cf513a32a62e1a
evanfrawley <frawley@uw.edu>
2017-10-16 23:21:25 -0700

    remove cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/evanfrawley/Github/personal/portfolio/src/layouts/index.js"
-  import data from "/Users/evanfrawley/Github/personal/portfolio/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
--- .cache/layouts/navLinks.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/evanfrawley/Github/personal/portfolio/src/layouts/navLinks.js"
-  import data from "/Users/evanfrawley/Github/personal/portfolio/.cache/json/layout-nav-links.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
